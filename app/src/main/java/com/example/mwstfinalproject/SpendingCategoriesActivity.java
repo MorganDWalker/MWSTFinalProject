89f28961584d44b8d066ffa2f39320feeff6a5f8
MorganDWalker <GreenPakman@gmail.com>
2021-12-06 22:55:35 -0500

    Added additional activity pages
    Added a table of buttons to the spending history page and added empty activities for other planned features

---
+++ app/src/main/java/com/example/mwstfinalproject/SpendingCategoriesActivity.java
@@ -0,0 +1,14 @@
+package com.example.mwstfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpendingCategoriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spending_categories);
+    }
+}
