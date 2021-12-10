78e94d8388c1505ed333b4df20f5e027ac9d5a4e
Steven Tripp <50451074+SJETInc@users.noreply.github.com>
2021-12-10 18:23:44 -0500

    Added navigation drawer to app

--- app/src/main/java/com/example/mwstfinalproject/MainActivity.java
+++ app/src/main/java/com/example/mwstfinalproject/MainActivity.java
@@ -1,20 +1,48 @@
 package com.example.mwstfinalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.TextView;
-
 import java.time.LocalDate;
+import android.view.View;
+import android.view.Menu;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mwstfinalproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
     TextView date;
     TextView total;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_spending_category, R.id.nav_spending_history, R.id.nav_add_expense)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
 
         DatabaseHelper dataBaseHelper = new DatabaseHelper(MainActivity.this);
 
@@ -24,5 +52,19 @@ public class MainActivity extends AppCompatActivity {
         date.setText(LocalDate.now().toString());
         total.setText("$" + dataBaseHelper.getTodaystTotal() + "\n Spent So Far");
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
---
+++ app/src/main/java/com/example/mwstfinalproject/ui/history/SpendingHistoryViewModel.java
@@ -0,0 +1,6 @@
+package com.example.mwstfinalproject.ui.history;
+
+import androidx.lifecycle.ViewModel;
+
+public class SpendingHistoryViewModel extends ViewModel {
+}
