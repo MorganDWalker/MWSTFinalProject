edff32607e899c76e89a44e6c576d40d5175ebdd
Steven Tripp <50451074+SJETInc@users.noreply.github.com>
2021-12-11 16:59:44 -0500

    Added floating button and navigation bar

--- app/src/main/java/com/example/mwstfinalproject/MainActivity.java
+++ app/src/main/java/com/example/mwstfinalproject/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.mwstfinalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import android.view.View;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
 
 import java.time.LocalDate;
 
@@ -16,7 +24,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("  Spending Tracker | Home");
         DatabaseHelper dataBaseHelper = new DatabaseHelper(MainActivity.this);
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.add_expense);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddExpenseActivity.class));
+            }
+        });
 
         date = findViewById(R.id.dateText);
         total = findViewById(R.id.dailySpendingTxt);
@@ -24,5 +42,25 @@ public class MainActivity extends AppCompatActivity {
         date.setText(LocalDate.now().toString());
         total.setText("$" + dataBaseHelper.getTodaystTotal() + "\n Spent So Far");
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu( Menu menu ) {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //Handles menu selection operations
+    @Override
+    public boolean onOptionsItemSelected( @NonNull MenuItem item ) {
+
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                Intent settings = new Intent(this, SettingsActivity.class);
+                startActivity(settings);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
