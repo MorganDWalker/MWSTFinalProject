96227d4bc831ddc91b169047f11b62c40e52946b
Steven Tripp <50451074+SJETInc@users.noreply.github.com>
2021-12-12 03:11:32 -0500

    Finalized Layout for "Add Expense" and "Settings" pages

--- app/src/main/java/com/example/mwstfinalproject/AddExpenseActivity.java
+++ app/src/main/java/com/example/mwstfinalproject/AddExpenseActivity.java
@@ -1,14 +1,80 @@
 package com.example.mwstfinalproject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.TextView;
+import android.app.DatePickerDialog;
+
+import java.util.Calendar;
 
 public class AddExpenseActivity extends AppCompatActivity {
+
+    DatePickerDialog picker;
+    EditText expenseCategory;
+    EditText expenseCost;
+    EditText expenseDate;
+    Button btnAddExpense;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expense);
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setTitle("  Spending Tracker | Add Expense");
+        DatabaseHelper dataBaseHelper = new DatabaseHelper(AddExpenseActivity.this);
+
+        expenseCategory = findViewById(R.id.txtExpenseCategory);
+        expenseCost = findViewById(R.id.txtExpenseCost);
+        expenseDate = findViewById(R.id.txtExpenseDate);
+        expenseDate.setInputType(InputType.TYPE_NULL);
+        //code for date selection
+        expenseDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                //date picker dialog
+                picker = new DatePickerDialog(AddExpenseActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                        expenseDate.setText(year + "-" + (monthOfYear + 1) + "-" + dayOfMonth);
+                    }
+                }, year, month, day);
+                picker.show();
+            }
+        });
+        //submit button code
+        btnAddExpense = findViewById(R.id.btnAddExpense);
+        btnAddExpense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+
+                startActivity(new Intent(AddExpenseActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void checkInput(){
+
     }
 }
--- app/src/main/java/com/example/mwstfinalproject/SettingsActivity.java
+++ app/src/main/java/com/example/mwstfinalproject/SettingsActivity.java
@@ -1,14 +1,36 @@
 package com.example.mwstfinalproject;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 import android.os.Bundle;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    Button btnClearData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setTitle("  Spending Tracker | Settings");
+        DatabaseHelper dataBaseHelper = new DatabaseHelper(SettingsActivity.this);
+
+        //submit button code
+        btnClearData = findViewById(R.id.btnClearData);
+        btnClearData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                //TODO - Add drop table action
+                startActivity(new Intent(SettingsActivity.this, MainActivity.class));
+            }
+        });
     }
 }
